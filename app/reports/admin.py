a5608bcff7287ed51fd36cdfed7a9837d04a2c41
Kamil Islamov <kam@kambox.ru>
2020-11-29 02:55:24 +0300

    totals

--- app/reports/admin.py
+++ app/reports/admin.py
@@ -8,7 +8,13 @@ from .models import *
 @admin.register(IQIndex)
 class IQIndexAdmin(ModelAdminTotals):
     list_display = [field.name for field in IQIndex._meta.get_fields()]
-    list_totals = [('iq_index', lambda field: Coalesce(Avg(field), 0)), ('digital', Avg), ('social', Avg), ('utility', Avg)]
+    list_totals = [('iq_index', lambda field: Coalesce(Avg(field), 0)),
+                   ('digital', lambda field: Coalesce(Avg(field), 0)),
+                   ('social', lambda field: Coalesce(Avg(field), 0)),
+                   ('utility', lambda field: Coalesce(Avg(field), 0)),
+                   ('area', lambda field: Coalesce(Sum(field), 0)),
+                   ('population', lambda field: Coalesce(Sum(field), 0)),
+                   ('vvp', lambda field: Coalesce(Sum(field), 0))]
     readonly_fields = list_display
 
 
